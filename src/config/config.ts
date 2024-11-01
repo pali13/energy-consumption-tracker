025dbf710f2d1b26f3b4000475cbc0da5b22f83a
palisuar13 <pali.bolso.13@gmail.com>
2024-10-31 21:22:54 -0300

    Probando cosas

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,11 +1,11 @@
 // const API_URL = 'http://192.168.1.27:8080'; // Reemplaza con la URL de tu backend
 // export default ;
 
-import Constants from "expo-constants";
-import devEnv from "../environments/environment";
-import prodEnv from "../environments/environment.prod";
+// import Constants from "expo-constants";
+// import devEnv from "../environments/environment";
+// import prodEnv from "../environments/environment.prod";
 
 // const ENV = Constants.manifest?.releaseChannel === "production" ? prodEnv : devEnv;
-const ENV = "https://energy-consumption-tracker.onrender.com:8080";
+const ENV = "https://energy-consumption-tracker.onrender.com";
 
 export default ENV;
